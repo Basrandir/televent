968ffd7c8cf428e472e65f997c989f63dba19786
Bassam Saeed <github@bassamsaeed.ca>
2024-03-12 16:39:57 -0400

    Initial bot setup

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use frankenstein::AllowedUpdate;
+use frankenstein::Api;
+use frankenstein::GetUpdatesParams;
+use frankenstein::ReplyParameters;
+use frankenstein::SendMessageParams;
+use frankenstein::TelegramApi;
+use frankenstein::UpdateContent;
+
+pub fn main() {
+    let token = std::env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
+    let api = Api::new(&token.to_string());
+
+    let update_params_builder = GetUpdatesParams::builder();
+    let mut update_params = update_params_builder.clone().build();
+
+    loop {
+        let result = api.get_updates(&update_params);
+
+        match result {
+            Ok(response) => {
+                for update in response.result {
+                    if let UpdateContent::Message(message) = update.content {
+                        // let reply_parameters = ReplyParameters::builder()
+                        //     .message_id(message.message_id)
+                        //     .build();
+
+                        let send_message_params = SendMessageParams::builder()
+                            .chat_id(message.chat.id)
+                            .text("hello")
+                            //   .reply_parameters(reply_parameters)
+                            .build();
+
+                        if let Err(err) = api.send_message(&send_message_params) {
+                            println!("Failed to send message: {err:?}");
+                        }
+                    }
+                    update_params = update_params_builder
+                        .clone()
+                        .offset(update.update_id + 1)
+                        .build();
+                }
+            }
+            Err(_) => {}
+        }
+    }
+}
